39d100fd6f2ed7bc84ee520232a39350ec63d3a9
Grad Laurentiu <gradlaur@yahoo.com>
2025-11-23 11:31:19 +0200

    Update ExitManager.cpp

--- Backend/device-data-simulator/ExitManager.cpp
+++ Backend/device-data-simulator/ExitManager.cpp
@@ -10,12 +10,13 @@ int ExitManager::exit(const Code exitCode)
 		return 0;
 	case FAILURE:
 		std::cout << "Exit failure!\n";
-		return -1;
+		return 1;
 	case NOTHING_PUBLISHED:
 		std::cout << "Nothing has been published!\nExit nothing done!\n";
-		return 1;
+		return 2;
 	case FAILED_TO_CONNECT:
 		std::cout << "Failed to connect to RabbitMQ!\nExit nothing done!\n";
-		return 2;
+		return 3;
 	}
+	return 255;
 }
